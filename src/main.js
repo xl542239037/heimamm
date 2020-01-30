e707f0b9f8c1c8088cecfcee103ec2d63da5e52f
笨笨的机器人 <18710143839@163.COM>
2020-01-30 10:36:05 +0800

    项目起步 - Element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由 router
 import router from './router/router'
+//导入elemrnt ui
+import Element from 'element-ui'
+//导入elm 样式
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+//注册
+Vue.use(Element)
 new Vue({
   render: h => h(App),
+  //挂载到Vue实例上 router
   router
 }).$mount('#app')
