44d303d26c1ea984798518b12d083edcd112eab7
笨笨的机器人 <18710143839@163.COM>
2020-02-01 18:10:36 +0800

    首页模块 - 路由设置

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,17 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import login from '../views/login/login.vue'
+import index from '../views/index/index.vue';
 const routes = [
+  //登录路由
   {
     path: '/login',
     component: login
+  },
+  //首页路由
+  {
+    path:'/index',
+    component: index
   }
 ]
 const router = new VueRouter({
